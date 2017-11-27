ffab2c99bb3aaeaca218c29ad9b8f4b68e61c35e
merin j <merinj@zipcoders-MacBook-Pro-7.local>
2017-11-27 16:35:31 -0500

    Merin

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,75 @@
 package io.zipcoder;
 
+import java.util.*;
+
 public class Problem4 {
+
+
+    public Map<Character,Integer> EachCharRepeat(String string) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < string.length(); i++) {
+            if (map.isEmpty()) {
+                map.put(string.charAt(i), 1);
+            } else {
+                if (map.containsKey(string.charAt(i))) {
+                    Integer value = map.get(string.charAt(i));
+                    map.remove(string.charAt(i));
+                    map.put(string.charAt(i), value + 1);
+                } else {
+                    map.put(string.charAt(i), 1);
+                }
+            }
+
+        }
+        return map;
+    }
+    public boolean canCreatePalindrom(Map<Character,Integer> map){
+        int even = 0, odd = 0;
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() % 2 == 0) {
+                even++;
+            }
+            else odd++;
+
+        }
+        if(odd == 1)
+            return true;
+        else
+            return false;
+    }
+    public String palindromString(Map<Character, Integer> linkedHashMap){
+        String string = "";
+        Map<String,Integer> map = null;
+        for (int i=linkedHashMap.size()-1;i<=0;i--){
+
+        }
+        return null;
+    }
+    public LinkedHashMap<Character, Integer> sortHashMapByValues(HashMap<Character, Integer> map) {
+        List<Character> mapKeys = new ArrayList<Character>(map.keySet());
+        List<Integer> mapValues = new ArrayList<Integer>(map.values());
+        Collections.sort(mapValues);
+        Collections.sort(mapKeys);
+
+        LinkedHashMap<Character, Integer> sortedMap = new LinkedHashMap<Character, Integer>();
+
+        Iterator<Integer> valueIt = mapValues.iterator();
+        while (valueIt.hasNext()) {
+            Integer val = valueIt.next();
+            Iterator<Character> keyIt = mapKeys.iterator();
+
+            while (keyIt.hasNext()) {
+                Character key = keyIt.next();
+                Integer comp1 = map.get(key);
+                Integer comp2 = val;
+
+                if (comp1.equals(comp2)) {
+                    keyIt.remove();
+                    sortedMap.put(key, val);
+                    break;
+                }
+            }
+        }
+        return sortedMap;
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,39 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Problem4Test {
+    Problem4 problem4 = new Problem4();
+    @Test
+    public void checkChangeToPalindTest(){
+        Map<Character,Integer> map = null;
+        String str = "cdefghmnopqrstuvw";
+        boolean expected = true;
+       map = problem4.EachCharRepeat(str);
+        System.out.println(map.toString());
+    }
+    @Test
+    public void canCreatePalindromTest(){
+        Map<Character,Integer> map = null;
+        boolean canCreatepalindrom = false;
+        String str = "cdefghmnopqrstuvw";
+        boolean expected = true;
+        map = problem4.EachCharRepeat(str);
+        canCreatepalindrom = problem4.canCreatePalindrom(map);
+        System.out.println(canCreatepalindrom);
+    }
+    @Test
+    public void sortHashMapByValuesTest(){
+        Map<Character,Integer> map = null;
+        boolean canCreatepalindrom = false;
+        String str = "cdcdcdcdeeeef";
+        boolean expected = true;
+        map = problem4.EachCharRepeat(str);
+        //System.out.println(problem4.sortHashMapByValues((HashMap<Character, Integer>) map));
+        problem4.palindromString( map);
+    }
 }
